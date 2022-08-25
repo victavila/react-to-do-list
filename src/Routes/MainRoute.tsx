e2488ba7cf70f8398a1461b37c7bfeb3d2484dfd
Victor Avila <victoravila1997@gmail.com>
2022-08-25 15:03:37 -0500

    Created main route

---
+++ src/Routes/MainRoute.tsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+import Important from '../components/Important/Important';
+import Inbox from '../components/Inbox/Inbox';
+import Project from '../components/Project/Project';
+import Today from '../components/Today/Today';
+import Upcoming from '../components/Upcoming/Upcoming';
+
+const MainRoute = () => {
+  return (
+    <Routes>
+      <Route path="/" element={< Inbox />} />
+      <Route path="/today" element={<Today />} />
+      <Route path="/upcoming" element={<Upcoming />} />
+      <Route path="/important" element={<Important />} />
+      <Route path="/:projectId" element={<Project />} />
+    </Routes>
+  )
+}
+
+export default MainRoute;

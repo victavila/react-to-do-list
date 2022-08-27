b02b61c47486858eca39b8dd53428b1402ba881f
Victor Avila <victoravila1997@gmail.com>
2022-08-27 15:48:36 -0500

    Created addToDo action and reducer

--- src/components/Todos/todoSlice.ts
+++ src/components/Todos/todoSlice.ts
@@ -1,11 +1,23 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { Todo } from '../../types/types';
+import { v4 as uuid } from 'uuid';
 
 const initialState: Todo[] = [];
 
-const todoslice = createSlice({
+const todoSlice = createSlice({
   name: "todos",
   initialState,
   reducers: {
+    addTodo(state, action: PayloadAction<string>) {
+      const newTodo: Todo = {
+        id: uuid(),
+        text: action.payload,
+      }
+      state.push(newTodo);
+    }
   }
 })
+
+export const { addTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;

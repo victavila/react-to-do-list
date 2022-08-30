26a5516573fecdf9fb2f27509013c825de427aa0
Victor Avila <victoravila1997@gmail.com>
2022-08-29 21:48:51 -0500

    Added icons to sidebar and made projects list collapsible

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 import { useAppSelector } from "../../app/hooks";
 import AddProject from "./AddProject/AddProject";
 import ProjectLink from "./ProjectLink/ProjectLink";
+import { FaInbox, FaRegCalendar, FaRegCalendarAlt, FaTag, FaChevronDown, FaChevronRight } from "react-icons/fa"
 
 const Sidebar = () => {
   const projects = useAppSelector(state => state.projects);
   const [formVisibility, setFormVisibility] = useState(false);
+  const [disable, setDisable] = useState(false);
 
   const toggleVisibility = () => {
     setFormVisibility(!formVisibility);
@@ -16,27 +18,52 @@ const Sidebar = () => {
     <nav className="sidebar">
       <ul className="links">
         <Link to="/">
-          <li className="nav-link">Inbox</li>
+          <li className="nav-link">
+            <span>
+              <FaInbox />
+            </span>
+             <span>Inbox</span>
+          </li>
         </Link>
         <Link to="/today">
-          <li className="nav-link">Today</li>
+          <li className="nav-link">
+            <span>
+              <FaRegCalendar />
+            </span>
+            <span>Today</span>
+          </li>
         </Link>
         <Link to="/upcoming">
-          <li className="nav-link">Upcoming</li>
+          <li className="nav-link">
+            <span>
+              <FaRegCalendarAlt />
+            </span>
+             <span>Upcoming</span>
+          </li>
         </Link>
         <Link to="/important">
-          <li className="nav-link">Important</li>
+          <li className="nav-link">
+            <span>
+              <FaTag />
+            </span>
+             <span>Important</span>
+          </li>
         </Link>
       </ul>
       <div className="nav-projects">
-        <h3>Projects</h3>
-        <ul>
+        <h3><span>
+          {disable ? 
+          <FaChevronRight onClick={() => {setDisable(!disable)}} /> 
+          : <FaChevronDown onClick={() => {setDisable(!disable)}} />}
+          </span>Projects</h3>
+        {disable ? <></> 
+        :<ul>
           {projects.map(project => (
             <Link key={project.id} to={`/${project.id}`}>
               <ProjectLink {...project} />
             </Link>
           ))}
-        </ul>
+        </ul>}
       </div>
       {formVisibility ? <AddProject toggleVisibility={toggleVisibility} />: <button onClick={toggleVisibility}>Add Project</button>}
     </nav>

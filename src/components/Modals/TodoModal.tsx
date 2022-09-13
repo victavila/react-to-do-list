32a34e0545e02b3539be2f98a5cc547573ee661d
Victor Avila <victoravila1997@gmail.com>
2022-09-13 00:00:27 -0500

    Close todo modal on overlay click

--- src/components/Modals/TodoModal.tsx
+++ src/components/Modals/TodoModal.tsx
@@ -1,17 +1,26 @@
 import { useContext } from "react";
 import { ProjectContext } from "../../contexts/ProjectContext";
+import { TodoModalContext } from "../../contexts/TodoModalContext";
 import AddTodo from "../Todos/components/AddTodo/AddTodo";
+import "./index.css";
 
 interface ProjectContextProps {
   selectedProject: string;
 }
 
+interface TodoModalContextProps {
+  setTodoModalOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+
 const TodoModal = () => {
-  const { selectedProject } = useContext(ProjectContext) as ProjectContextProps
+  const { selectedProject } = useContext(ProjectContext) as ProjectContextProps;
+  const { setTodoModalOpen } = useContext(TodoModalContext) as TodoModalContextProps;
   
   return (
-    <div className="todo-modal">
-      <AddTodo project={selectedProject} />
+    <div className="todo-modal" onClick={() => setTodoModalOpen(false)}>
+      <div onClick={e => e.stopPropagation()}>
+        <AddTodo project={selectedProject} />
+      </div>
     </div>
   )
 }
